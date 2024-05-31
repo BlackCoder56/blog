8eac9bb7dcd0033567a1535e2b544425b19cd512
BlackCoder56 <blackcoda56@gmail.com>
2024-05-31 22:22:22 +0300

    third commit

--- nileblog/admin.py
+++ nileblog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . models import Article, Like, Comment
 
 # Register your models here.
+admin.site.register(Article)
+admin.site.register(Like)
+admin.site.register(Comment)
--- nileblog/models.py
+++ nileblog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title_text = models.CharField(max_length=50)
+    preview_text = models.CharField(max_length=100)
+    details_text = models.CharField(max_length=500)
+    pub_date = models.DateTimeField("date published")
+    
+    def __str__(self):
+        return self.title_text
+    
+class Like(models.Model):
+    title_text = models.ForeignKey(Article, on_delete=models.CASCADE)
+    upvote = models.IntegerField(default=0)
+    downvote = models.IntegerField(default=0)
+    
+    
+class Comment(models.Model):
+    title_text = models.ForeignKey(Article,  on_delete=models.CASCADE)
+    comment_text = models.CharField(max_length=1000)
